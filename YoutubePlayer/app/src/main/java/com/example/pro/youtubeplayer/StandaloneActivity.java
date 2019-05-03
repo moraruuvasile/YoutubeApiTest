f64fa74ba95479a4da82c789c824bbc5c5036b5c
Vasile Moraru <vaseoa@yahoo.com>
2019-05-03 17:34:58 +0300

    Add files via upload

---
+++ YoutubePlayer/app/src/main/java/com/example/pro/youtubeplayer/StandaloneActivity.java
@@ -0,0 +1,43 @@
+package com.example.pro.youtubeplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_standalone);
+
+		Button btnPlayVideo = findViewById(R.id.btnPlayVideo);
+		Button btnPlayList = findViewById(R.id.btnPlayList);
+
+		btnPlayVideo.setOnClickListener(this);
+		btnPlayList.setOnClickListener(this);
+
+
+	}
+
+	@Override
+	public void onClick(View view) {
+		Intent intent = null;
+
+		switch (view.getId()){
+			case R.id.btnPlayVideo:
+				intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_VIDEO_ID);
+				break;
+			case R.id.btnPlayList:
+				intent = YouTubeStandalonePlayer.createPlaylistIntent(this, YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_PLAYLIST);
+				break;
+		}
+
+		if(intent != null)
+			startActivity(intent);
+	}
+}
